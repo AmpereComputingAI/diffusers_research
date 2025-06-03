b39fd6683d6978ce708b4c110f9b0a7f9a5a5b32
Jan Grzybek <jan@amperecomputing.com>
2025-06-04 01:31:25 +0200

    wip

--- run_sd.py
+++ run_sd.py
@@ -503,10 +503,10 @@ class Tracer:
             print("------")
             print(op.name)
             print(op.location)
+            print({key: [val.type, val.hash, val.meta] for key, val in op.args.items()})
             print({key: [val.type, val.hash, val.meta] for key, val in op.input.items()})
             print({key: [val.type, val.hash, val.meta] for key, val in op.output.items()})
-            print("Section:")
-            print(" -> ".join([str(section.annotation)[:20] for section in op.section_stack]))
+            print(" -> ".join([str(section.annotation)[:20].strip("\n") for section in op.section_stack]))
             # print("Conditions:")
             # for cond in op.condition_stack:
             #     print(cond.condition)
