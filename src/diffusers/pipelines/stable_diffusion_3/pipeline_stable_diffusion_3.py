fee4ccc7b447a16aa79f2633c877ea645cad1c95
Jan Grzybek <jan@amperecomputing.com>
2025-06-03 21:00:35 +0200

    wip

--- src/diffusers/pipelines/stable_diffusion_3/pipeline_stable_diffusion_3.py
+++ src/diffusers/pipelines/stable_diffusion_3/pipeline_stable_diffusion_3.py
@@ -262,17 +262,19 @@ class StableDiffusion3Pipeline(DiffusionPipeline, SD3LoraLoaderMixin, FromSingle
             tracer.add_op("torch.zeros", input_dict, {"output": x})
             return x
 
-        text_inputs = self.tokenizer_3(
-            tracer,
-            prompt,
-            padding="max_length",
-            max_length=max_sequence_length,
-            truncation=True,
-            add_special_tokens=True,
-            return_tensors="pt",
-        )
+        with tracer.section(self.tokenizer_3):
+            text_inputs = self.tokenizer_3(
+                tracer,
+                prompt,
+                padding="max_length",
+                max_length=max_sequence_length,
+                truncation=True,
+                add_special_tokens=True,
+                return_tensors="pt",
+            )
         text_input_ids = text_inputs.input_ids
-        untruncated_ids = self.tokenizer_3(tracer, prompt, padding="longest", return_tensors="pt").input_ids
+        with tracer.section(self.tokenizer_3):
+            untruncated_ids = self.tokenizer_3(tracer, prompt, padding="longest", return_tensors="pt").input_ids
 
         if untruncated_ids.shape[-1] >= text_input_ids.shape[-1] and not torch.equal(text_input_ids, untruncated_ids):
             removed_text = self.tokenizer_3.batch_decode(untruncated_ids[:, self.tokenizer_max_length - 1 : -1])
