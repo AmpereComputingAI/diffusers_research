697ba90e885a179b6186da8f76e048adfa3ae844
Jan Grzybek <jan@amperecomputing.com>
2025-06-08 01:26:27 +0200

    wip

--- run_sd.py
+++ run_sd.py
@@ -469,8 +469,8 @@ class Graph:
         self.vars = {}
 
     class Variable:
-        #codes = [str(i) for i in range(10000)]
-        codes = list(string.ascii_uppercase)
+        codes = [str(i) for i in range(10000)]
+        #codes = list(string.ascii_uppercase)
         occupied_codes = []
 
         def __init__(self, dependants):
