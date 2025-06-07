d186dd97fd9163fd6039e29251de78292bc44bea
Jan Grzybek <jan@amperecomputing.com>
2025-06-07 23:34:53 +0200

    wip

--- run_sd.py
+++ run_sd.py
@@ -508,6 +508,9 @@ class Graph:
         while len(self.ops) - len(processed) > 0:
             for op in self.ops:
                 if all([dep in processed for dep in op.dependencies]):
+                    print(op.name)
+                    print(op.location)
+                    print(self.vars)
                     outputs = [self.new_var(tensor, op.dependants) for tensor in op.output_tensors]
                     if len(outputs) > 0:
                         outputs = ", ".join(outputs) + " = "
@@ -593,15 +596,15 @@ class Tracer:
     def summary(self):
         tensor_map = {tensor.output_tensors[0]: tensor for tensor in self.preloaded_tensors}
         for op in self.ops:
-            print("------")
-            print(op.name)
-            print(op.location)
-            print({key: [val.type, val.hash, val.meta] for key, val in op.args.items()})
-            print({key: [val.type, val.hash, val.meta] for key, val in op.input.items()})
-            print({key: [val.type, val.hash, val.meta] for key, val in op.output.items()})
-            print(op.input_tensors)
-            print(op.output_tensors)
-            print(" -> ".join([str(section.annotation).split("(")[0] for section in op.section_stack]))
+            # print("------")
+            # print(op.name)
+            # print(op.location)
+            # print({key: [val.type, val.hash, val.meta] for key, val in op.args.items()})
+            # print({key: [val.type, val.hash, val.meta] for key, val in op.input.items()})
+            # print({key: [val.type, val.hash, val.meta] for key, val in op.output.items()})
+            # print(op.input_tensors)
+            # print(op.output_tensors)
+            # print(" -> ".join([str(section.annotation).split("(")[0] for section in op.section_stack]))
 
             if len(op.input_tensors) > 0:
                 for tensor in op.input_tensors:
